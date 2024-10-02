1a8e087630660b65a54e2bf19da50e1d3c2f5c5e
Joshua Willis <joshua.willis@intellitect.com>
2024-10-01 19:34:06 -0700

    Add S3 Terraform module

---
+++ infra/modules/s3/main.tf
@@ -0,0 +1,15 @@
+locals {
+  default_tags = var.default_tags
+  name_prefix  = var.name_prefix
+
+  bucket_name = "s3-${local.name_prefix}-${var.bucket_name}"
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = local.bucket_name
+
+  tags = merge(
+    local.default_tags,
+    { Name = local.bucket_name }
+  )
+}
